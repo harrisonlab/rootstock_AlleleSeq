3972baeceac0ba24040973a4e610ec4db0ee7b8c
Greg Deakin <GregDeakin@users.noreply.github.com>
2015-04-08 16:42:20 +0100

    Create pileup2.sh

---
+++ pileup2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(readlink -f ${0%/*})
+
+REF=$1
+BAMS=$2
+DEST=$3
+OUTNAME=$4
+REGIONS=$5
+
+count=0
+while read REGION
+do
+	let count++
+	qsub $SCRIPT_DIR/submit_pileup2.sh $REF $BAMS $DEST $OUTNAME.$count $REGION
+done < $REGIONS
