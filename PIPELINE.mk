22963506df7deb8a8d01b755a4e992038f68b743
Greg Deakin <GregDeakin@users.noreply.github.com>
2015-04-28 09:08:46 +0100

    Update PIPELINE.mk

--- PIPELINE.mk
+++ PIPELINE.mk
@@ -1,23 +1,22 @@
 
-BASE=/home/groups/harrisonlab/project_files/rootstock_genetics/m116/allele
+BASE=xxx
 
 PL:=.
-SNPS:=$(BASE)/m116.snv
-CNVS:=$(BASE)/m116.snv.cnv 
+SNPS:=$(BASE)/x.snv
+CNVS:=$(BASE)/x.snv.cnv 
 BNDS:=hits.bed
-MAPS:=$(BASE)/chr%s_m116.map
+MAPS:=$(BASE)/chr%s_x.map
 FDR_SIMS:=5
 FDR_CUTOFF:=0.1
 
 
+sourcefiles := $(RNA_1)
+countfiles := $(subst .fq.trim,.cnt,$(sourcefiles))
 
-sourcefiles := $(wildcard *.fastq.gz)
-countfiles := $(subst .fastq.gz,.cnt,$(sourcefiles))
-
-%.cnt:%.fastq.gz
+%.cnt:%.fq.trim 	
 	bash -c "python $(PL)/MergeBowtie.py \
-           <($(PL)/filter_input.sh $(PL) $< | bowtie --best --strata -v 2 -m 1 -f $(BASE)/pat/m116_paternal_index - ) \
-           <($(PL)/filter_input.sh $(PL) $< | bowtie --best --strata -v 2 -m 1 -f $(BASE)/mat/m116_maternal_index - ) \
+           <($(PL)/filter_input.sh $(PL) $(RNA_1) $(RNA_2) a1; bowtie2 --no-hd --no-unal -f -x $(BASE)/pat2/m116_paternal_index -1 a1_1 -2 a1_2|grep -v XS |cut -f 1,2,3,4,10) \
+           <($(PL)/filter_input.sh $(PL) $(RNA_1) $(RNA_2) a2; bowtie2 --no-hd --no-unal -f -x $(BASE)/mat2/m116_maternal_index -1 a2_1 -2 a2_2|grep -v XS |cut -f 1,2,3,4,10) \
            $(MAPS) | python $(PL)/SnpCounts.py $(SNPS) - $(MAPS) $@"
 
 all: interestingHets.txt
