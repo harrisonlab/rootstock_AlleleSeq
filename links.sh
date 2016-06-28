be168500aa87f138db210013d7588d2476113386
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-06-28 15:02:11 +0100

    Update links.sh

--- links.sh
+++ links.sh
@@ -1,3 +1,4 @@
+ ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/11-RNA_L1_1.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/11-RNA_L1_2.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/12-RNA_L1_1.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/12-RNA_L1_2.fq.trim
@@ -19,6 +20,9 @@
  ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/04-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/04-RNA_L1_2.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/05-RNA_L1_1.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/05-RNA_L1_2.fq.trim
+ 
+ ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/11-RNA_L1_1.fq.trim
+ ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/11-RNA_L1_2.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/12-RNA_L1_1.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/12-RNA_L1_2.fq.trim
  ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/13-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/13-RNA_L1_1.fq.trim
