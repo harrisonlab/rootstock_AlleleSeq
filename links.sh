4dc35f9eb4e46124c80e9488024e1142c4adc5c0
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-06-29 09:27:49 +0100

    Update links.sh

--- links.sh
+++ links.sh
@@ -1,45 +1,26 @@
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/11-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/11-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/12-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/12-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/13-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/13-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/13-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/13-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/06-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/06-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/06-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/06-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/07-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/07-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/07-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/07-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/08-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/08-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/09-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/09-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/09-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/09-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/08-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/08-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/02-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/02-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/02-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/02-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/03-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/03-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/03-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/03-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/04-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/04-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/04-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/04-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/05-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/05-RNA_L1_2.fq.trim
  
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/11-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/11-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/12-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/12-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/13-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/13-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/13-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/13-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/06-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/06-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/06-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/06-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/07-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/07-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/07-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/07-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/08-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/08-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/09-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/09-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/09-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/09-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/08-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/08-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/02-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/02-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/02-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/02-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/03-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/03-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/03-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/03-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/04-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/04-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/04-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/04-RNA_L1_2.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/05-RNA_L1_1.fq.trim
- ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/05-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/02-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/02-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/02-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/02-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/03-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/03-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/03-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/03-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/04-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/04-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/04-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/04-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/05-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_3/fastq/05-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/05-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/06-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/06-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/06-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/06-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/07-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/07-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/07-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m27/allele_seq/07-RNA_L1_2.fq.trim
+
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/08-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/08-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/08-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/08-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/09-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/09-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_2/fastq/09-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/09-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/10-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/10-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/10-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/10-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/11-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/11-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/11-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/12-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/12-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/12-RNA_L1_2.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/13-RNA_L1_1.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/13-RNA_L1_1.fq.trim
+ln -s $ROOTSTOCK/rna-seq/RNAseq_1/fastq/13-RNA_L1_2.fq.gz.trimmed.fq $ROOTSTOCK/allele/m116/allele_seq/13-RNA_L1_2.fq.trim
