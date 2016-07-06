f7c9528d2bd58545c9e9d42063e4a2ce773ec746
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-07-06 10:43:23 +0100

    Update figure_3.R

--- figure_3.R
+++ figure_3.R
@@ -8,7 +8,7 @@ between <- function(X,pos,dist) {
 	,]
 }
 
-within <- function(X,pos) {
+within <- function(X,pos,bin) {
 	X[
 		(X$direction=="forward"&X[,1]<=pos&X[,2]>=pos) |
 		(X$direction=="reverse"&X[,2]<=pos&X[,1]>=pos)
@@ -37,7 +37,39 @@ ev <- function(p,a) {
 	}
 }
 
-genes <- read.table("qtl13_genes",header=T,sep="\t")
+get_data <- function(sname,loc,alpha=0.05,location=within,dist=500) {
+	options(stringsAsFactors = FALSE)
+	library(plyr)
+	mytable <- read.table(paste(sname,".txt",sep=""),header=T,sep="\t")
+	#genes could also be a list of exons
+	genes <- read.table(loc,header=T,sep="\t")
+	# Set probability of Weird results to 1 (they're too weird to use)
+	t1 <- mytable
+	t1[grep("Weird",t1[,9]),12] <- 1
+	t1[grep("Weird",t1[,10]),13] <- 1
+	t1[grep("Weird",t1[,11]),14] <- 1
+	# sum samples with allele-seq probability less than or equal to alpha 
+	#alpha = 0.05
+	t1$evidence <- apply(t1[,12:14],1,function(x) return(ev(x[1],alpha)+ev(x[2],alpha)+ev(x[3],alpha)))
+	# Select phased snps with at least 2 samples with significant allelic expression 
+	# (selecting on column 2 (phase) is not strictly necessary as only phased snps will have a probability less than 1)
+	phased <- t1[t1[,2]=="PHASED"&t1$evidence>=2,] 
+	#list of genes and exons containing phased snps
+	##produces list of data.frames of 0 or greater rows
+	phased_genes <- apply(phased,1,function(x) location(genes,x[1],dist))
+	##combine into a single data.frame (dumps 0 length rows)
+	phased_genes <- ldply(phased_genes,data.frame)
+	##collapse to  unique plus number of snps per gene
+	unique_phased_genes <- ddply(phased_genes,.(Gene_ID),nrow)
+	colnames(unique_phased_genes)[2] <- "snp_count"
+	unique_phased_genes <- merge(unique_phased_genes,genes,all.x=T)
+	xx <- paste(sname,"_phased_genes.txt",sep="")	
+	write.table(unique_phased_genes,xx,sep="\t",quote=F,row.names=F)
+	mylist <- list(mytable,unique_phased_genes)
+	names(mylist) <- c("allele_seq","sig_phased")
+	return(mylist)
+}
+
 exons <- read.table("qtl_exons",header=T,sep="\t")
 
 #m27_chr5.txt
@@ -47,41 +79,16 @@ exons <- read.table("qtl_exons",header=T,sep="\t")
 #m116_chr11.txt
 #m116_chr13.txt
 
+m27_pg_q5 <-  get_data("m27_chr5","qtl5_genes",0.05)
+m27_pg_q11 <- get_data("m27_chr11","qtl11_genes",0.05)
+m27_pg_q13 <- get_data("m27_chr13","qtl13_genes",0.05)
+m116_pg_q5 <- get_data("m116_chr5","qtl5_genes",0.05)
+m116_pg_q11 <- get_data("m116_chr11","qtl11_genes",0.05)
+m116_pg_q13 <- get_data("m116_chr13""qtl13_genes",,0.05)
 
-x="m27_chr13"
-t1 <- read.table(paste(x,".txt",sep=""),header=T,sep="\t")
-
-# sum samples with allele-seq probability less than or equal to alpha 
-alpha = 0.05
-t1$evidence <- apply(t1[,12:14],1,function(x) return(ev(x[1],alpha)+ev(x[2],alpha)+ev(x[3],alpha)))
-
-# Select phased snps with at least 2 samples with significant allelic expression 
-# (selecting on column 2 (phase) is not strictly necessary as only phased snps will have a probability less than 1)
-phased <- t1[t1[,2]=="PHASED"&t1$evidence>=2,] 
-
-#list of genes and exons containing phased snps
-##produces list of data.frames of 0 or greater rows
-phased_genes <- apply(phased,1,function(x) within(genes,x[1]))
-##combine into a single data.frame (dumps 0 length rows)
-phased_genes <- ldply(phased_genes,data.frame)
-##collapse to  unique plus number of snps per gene
-unique_phased_genes <- ddply(phased_genes,.(Gene_ID),nrow)
-colnames(unique_phased_genes)[2] <- "snp_count"
-unique_phased_genes <- merge(unique_phased_genes,genes,all.x=T)
-xx <- paste(x,"_phased_genes.txt",sep="")
-write.table(unique_phased_genes,xx,sep="\t",quote=F,row.names=F)
-
-### This is fairly pointless as the objects have already been created (but overwritten) - but as I've already saved the genes...
-m27_pg_q5 <- read.table("m27_chr5_phased_genes.txt",header=T,sep="\t")
-m27_pg_q11 <- read.table("m27_chr11_phased_genes.txt",header=T,sep="\t")
-m27_pg_q13 <- read.table("m27_chr13_phased_genes.txt",header=T,sep="\t")
-m116_pg_q5 <- read.table("m116_chr5_phased_genes.txt",header=T,sep="\t")
-m116_pg_q11 <- read.table("m116_chr11_phased_genes.txt",header=T,sep="\t")
-m116_pg_q13 <- read.table("m116_chr13_phased_genes.txt",header=T,sep="\t")
-
-q5_uncom <-  m27_pg_q5[!m27_pg_q5[,1]%in%m116_pg_q5[,1],] 
-q11_com <- merge(m27_pg_q11,m116_pg_q11,by="Gene_ID")
-q13_com <- merge(m27_pg_q13,m116_pg_q13,by="Gene_ID")
+q5_uncom <-  m27_pg_q5$sig_phased[!m27_pg_q5$sig_phased[,1]%in%m116_pg_q5$sig_phased[,1],] 
+q11_com <- merge(m27_pg_q11$sig_phased,m116_pg_q11$sig_phased,by="Gene_ID")
+q13_com <- merge(m27_pg_q13$sig_phased,m116_pg_q13$sig_phased,by="Gene_ID")
 
 ##get unique phased exons
 phased_exons <- apply(phased,1,function(x) within(exons,x[1]))
