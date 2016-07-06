17103686640d318b14cd5078c1cd16c43f93ade6
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-07-06 17:06:22 +0100

    Create plotter.R

---
+++ plotter.R
@@ -0,0 +1,17 @@
+### 1. this finds the number of matches between m116 c1 and m27.m9 in each run of 10 snps
+
+### 2. then should look for stretches above a score of 8, then switch to m27.m13 when score goes below 2 
+### and switch back when above 8 again
+
+#1
+phased_chr <- read.table("m116_phased_chr5.txt",header=T,sep="\t")
+X <- phased_chr
+X$tr <- (X[,4]==X[,3])
+#cumulitive sum of differences between m116 c1 and m27.m9
+csvec <- cumsum(X$tr)
+# filter =  1  0  0  0  0  0  0  0  0  0 -1
+f11 <- c(1,rep(0,9),-1)
+# apply filter 
+fv <- filter(csvec, f11, sides = 1)
+
+#2
