8907324b7a5d922a0f940d7f7e83511213fdcb1d
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-07-07 15:40:15 +0100

    Update phasing.r

--- phasing.r
+++ phasing.r
@@ -8,6 +8,7 @@ m27_trio <- remove_hom_child(m27_trio)
 m27_trio <- remove_all_het(m27_trio)
 phased_m27 <- cbind(m27_trio[,1:4],t(as.data.frame(phaser(m27_trio[,5:10]))))
 colnames(phased_m27) <- c("chr","pos","ref","alt","m9.m27","m13.m27")
+write.table(phased_m27,"phased_m27",sep="\t",row.names=F,quote=F)
 
 m116_trio <- snps[,c(1:4,11:12,7:8,9:10)]
 m116_trio <- remove_impossible(m116_trio)
@@ -15,6 +16,7 @@ m116_trio <- remove_hom_child(m116_trio)
 m116_trio <- remove_all_het(m116_trio)
 phased_m116 <- cbind(m116_trio[,1:4],t(as.data.frame(phaser(m116_trio[,5:10]))))
 colnames(phased_m116) <- c("chr","pos","ref","alt","mm106.m116","m27.m116")
+write.table(phased_m116,"phased_m116",sep="\t",row.names=F,quote=F)
 
 all_phased <- merge(phased_m116,phased_m27,all=T)
 all_phased[is.na(all_phased)] <- 0
@@ -49,6 +51,7 @@ m116_trio <- remove_all_het(m116_trio)
 phased_m116 <- t(as.data.frame(phaser(m116_trio[,5:10])))
 fullphase<- cbind(m116_trio[,1:6],phased_m116[,1])
 colnames(fullphase)[7] <- "m116"
+write.table(fullphase,"m116_m13.m9",sep="\t",row.names=F,quote=F)
  
 remove_impossible <- function(X) {
 	X <- X[
@@ -88,19 +91,21 @@ phaser <- function(X) {
 		if(x[5]==x[3]){count=count+2}
 		if(x[5]==x[2]){count=count+4}
 		if(x[5]==x[1]){count=count+8}
-		if(x[5]==1) {
+		#if(x[5]>x[6]) {
 			if(count==1|count==2|count==3|count==7|count==11){
-				return(c(0,1))
+				return(as.numeric(c(x[6],x[5])))
+				#return(c(0,1))
 			} else {
-				return(c(1,0))
+				return(as.numeric(c(x[5],x[6])))
+				#return(c(1,0))
 			}
-		} else {
-			if(count==1|count==2|count==3|count==7|count==11){
-				return(c(1,0))
-			} else {
-				return(c(0,1))
-			}
-		}
+		#} else {
+		#	if(count==1|count==2|count==3|count==7|count==11){
+		#		#return(c(1,0))
+		#	} else {
+		#		#return(c(0,1))
+		#	}
+		#}
 	})
 }
 
