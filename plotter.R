8a2566b1cbb6b00eeff8245dc103fb24ad76cd44
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-07-08 09:36:24 +0100

    Update plotter.R

--- plotter.R
+++ plotter.R
@@ -1,4 +1,4 @@
-### 1. this finds the number of matches between m116 c1 and m27.m9 in each run of 10 snps
+### 1. this finds the number of matches between m116 c1 and m27.m9 in each run of 20 snps
 
 ### 2. then should look for stretches above a score of 8 (i.e. inheritted from m13, then switch to m27.m9 when score goes below 2 
 ### and switch back when above 8 again
@@ -17,15 +17,15 @@ myc$tr <- (myc[,7]==myc[,6])
 # cumulitive sum of differences between m116 c5 and m27.m13
 csvec <- cumsum(myc$tr)
 # filter =  1  0  0  0  0  0  0  0  0  0 -1
-f11 <- c(1,rep(0,9),-1)
+f11 <- c(1,rep(0,19),-1)
 # apply filter 
 fv <- filter(csvec, f11, sides = 1)
 # first 9 entries must be NA, set them to 5 
-fv[is.na(fv)] <-5
+fv[is.na(fv)] <-10
 
 mv <- 0
 #2 get +8 sequences
-myc$new <- sapply(fv,function(x) if(x>=8){mv<<-1;return(mv)}else if(x<=2){mv<<-2;return(mv)}else if(x==5){mv<<-0;return(mv)}else{return(mv)})
+myc$new <- sapply(fv,function(x) if(x>=15){mv<<-1;return(mv)}else if(x<=5){mv<<-2;return(mv)}else if(x==10){mv<<-0;return(mv)}else{return(mv)})
 # find postion where sequence changes
 change <- myc[c(1,1+which(diff(myc$new)!=0)),]
 # get stop and start positions from change - can't think of a good R way of doing this so will use a for loop
@@ -38,10 +38,10 @@ for (i in 1:(length(change[,1])-1)) {
 }
 
 #3 plotting
-pdf("test.pdf",width=1,height=10)  
-plot(NULL, xlim=c(0.8,1.3),ylim=c(0,max(df[,2])))
+pdf("chr5.pdf",width=1.5,height=6)  
+plot(NULL, xlim=c(0.8,1.3),ylim=c(0,max(phased_chr[,2])),xaxt='n',yaxt='n',ann=F)
 #,at=c(1,2,3,4),labels=c("ca1_p1","ca1_p2","ca2_p1","ca2_p2")
-apply(df[df$inht==1,],1,function(x) segments(0.9,as.numeric(x[1]),0.9,as.numeric(x[2]),col=1,lwd=4))
-apply(df[df$inht==2,],1,function(x) segments(1.2,as.numeric(x[1]),1.2,as.numeric(x[2]),col=2,lwd=4))
+apply(df[df$inht==1,],1,function(x) segments(0.9,as.numeric(x[1]),0.9,as.numeric(x[2]),col=1,lwd=3))
+apply(df[df$inht==2,],1,function(x) segments(1.2,as.numeric(x[1]),1.2,as.numeric(x[2]),col=2,lwd=3))
 dev.off()
 
